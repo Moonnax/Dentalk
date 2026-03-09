74977487b7b26517b373a163b8714af96cfa4691
Luana Oliveira <luana.2327891@gmail.com>
2026-03-08 21:45:29 -0300

    menu hamburguer - medium responsive - teste feito na página sobre

--- js/menuToggle.js
+++ js/menuToggle.js
@@ -1,6 +1,18 @@
 const menuToggle = document.getElementById("menuToggle");
 const nav = document.getElementById("nav");
 
-menuToggle.addEventListener("click", function () {
+menuToggle.addEventListener("click", function(){
+
     nav.classList.toggle("active");
+
+    const icon = menuToggle.querySelector("svg");
+
+    if(nav.classList.contains("active")){
+        icon.setAttribute("data-lucide","x");
+    }else{
+        icon.setAttribute("data-lucide","menu");
+    }
+
+    lucide.createIcons();
+
 });
