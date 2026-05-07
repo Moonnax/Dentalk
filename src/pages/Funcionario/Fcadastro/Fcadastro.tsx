3b9e52dd2e77c18b70a417a1e460c7636e9c9dbc
Luana Oliveira <luana.2327891@gmail.com>
2026-05-07 20:13:47 -0300

    pagina cadastrods padronizando cores e tabela

--- src/pages/Funcionario/Fcadastro/Fcadastro.tsx
+++ src/pages/Funcionario/Fcadastro/Fcadastro.tsx
@@ -1,156 +1,151 @@
-import HeaderFincionario from "../../../components/HeaderFuncionario/HeaderFuncionario";
+import {
+  ChevronDown,
+  Plus,
+  Search,
+} from "lucide-react";
+
+import HeaderFuncionario from "../../../components/HeaderFuncionario/HeaderFuncionario";
 import Footer from "../../../components/Footer/Footer";
-import "./Fcadastro.css";
-import { Link } from "react-router-dom";
-import { ChevronDown, Search, Plus } from "lucide-react";
 
-function Fcadastro() {
+import CardUsuarioFuncionario from "../../../components/CardUsuarioFuncionario/CardUsuarioFuncionario";
+
+import "./FCadastro.css";
+
+export default function CadastroF() {
+
+  const usuarios = [
+    {
+      nome: "Amanda Ribeiro Costa",
+      idade: "24 anos",
+      status: "Ativo",
+      categoria: "Funcionário",
+    },
+    {
+      nome: "Gabriel Henrique Souza",
+      idade: "19 anos",
+      status: "Encaminhado",
+      categoria: "Beneficiário",
+    },
+    {
+      nome: "Larissa Mendes Oliveira",
+      idade: "33 anos",
+      status: "Atendimento",
+      categoria: "Beneficiário",
+    },
+    {
+      nome: "Camila Rocha Alves",
+      idade: "41 anos",
+      status: "Finalizado",
+      categoria: "Beneficiário",
+    },
+    {
+      nome: "Eduardo Martins Ferreira",
+      idade: "38 anos",
+      status: "Ativo",
+      categoria: "Funcionário",
+    },
+  ];
+
   return (
-    <>
-      <HeaderFincionario />
+    <div className="page-container">
 
-      <main className="cadastro_container">
+      <HeaderFuncionario />
 
-        <section className="filtros_cadastro">
-          <div className="filtros_grupo">
+      <main className="container_cadastro">
 
-            <div className="caixa_filtro">
-              Categoria <ChevronDown size={16} />
+        {/* TOPO */}
+        <section className="topo_cadastro">
+
+          <div className="lado_esquerdo_topo">
+
+            <div className="barra_pesquisa">
+              <Search size={18} />
+              <p>Pesquisar cpf ou nome...</p>
             </div>
 
-            <div className="caixa_filtro">
-              Status <ChevronDown size={16} />
-            </div>
+            <div className="filtros_flex">
 
-            <div className="caixa_filtro">
-              Idade <ChevronDown size={16} />
-            </div>
+              <div className="filtro_item">
+                <span>Categoria</span>
+                <ChevronDown size={18} />
+              </div>
 
-            <div className="simular_busca">
-              <Search size={16} />
-              <span>Pesquisar cpf ou nome...</span>
+              <div className="filtro_item">
+                <span>Status</span>
+                <ChevronDown size={18} />
+              </div>
+
+              <div className="filtro_item">
+                <span>Idade</span>
+                <ChevronDown size={18} />
+              </div>
+
             </div>
 
           </div>
 
-          <button className="botao_laranja">
-            <Plus size={16} />
+          <button className="btn_novo_cadastro">
+            <Plus size={18} />
             Novo Cadastro
           </button>
+
         </section>
 
         <section className="contagem_abas">
 
-          <div className="aba">
-            Todos <span className="bolinha_padrao">284</span>
+          <div className="aba_item">
+            Todos
+            <span className="badge_todos">284</span>
           </div>
 
-          <div className="aba">
-            Voluntário <span className="bolinha_padrao">100</span>
+          <div className="aba_item">
+            Voluntários
+            <span className="badge_voluntario">100</span>
           </div>
 
-          <div className="aba">
-            Beneficiário <span className="bolinha_padrao">184</span>
+          <div className="aba_item">
+            Beneficiários
+            <span className="badge_beneficiario">184</span>
+          </div>
+
+        </section>
+
+        <section className="lista_usuarios_secao">
+
+          <div className="card_lista_usuarios">
+
+            <div className="linha_usuario header_usuario">
+
+              <span className="col_nome">Nome</span>
+
+              <span className="col_idade">Idade</span>
+
+              <span className="col_status">Status</span>
+
+              <span className="col_categoria">Categoria</span>
+
+              <span className="col_detalhes">Detalhes</span>
+
+            </div>
+
+            {usuarios.map((usuario, index) => (
+              <CardUsuarioFuncionario
+                key={index}
+                nome={usuario.nome}
+                idade={usuario.idade}
+                status={usuario.status}
+                categoria={usuario.categoria}
+              />
+            ))}
+
           </div>
 
         </section>
 
-        <section className="lista_usuarios">
-
-          <div className="linha_cabecalho">
-            <span className="col-nome-c">Nome</span>
-            <span className="col-idade-c">idade</span>
-            <span className="col-status-c">Status</span>
-            <span className="col-categoria-c">Categoria</span>
-            <span className="col-detalhes-c">Detalhes</span>
-          </div>
-
-          <div className="linha_usuario">
-            <span className="col-nome-c">Rafael Souza Lima</span>
-            <span className="col-idade-c">31 anos</span>
-            <span className="col-status-c">Ativo</span>
-            <span className="col-categoria-c">Funcionario</span>
-            <Link className="col-detalhes-c link_fake" to="#">ver mais</Link>
-          </div>
-
-          <div className="linha_usuario destaque">
-            <span className="col-nome-c">Mariana Costa Fernandes</span>
-            <span className="col-idade-c">28 anos</span>
-            <span className="col-status-c">Encaminhado</span>
-            <span className="col-categoria-c">Beneficiário</span>
-            <Link className="col-detalhes-c link_fake" to="#">ver mais</Link>
-          </div>
-
-          <div className="linha_usuario">
-            <span className="col-nome-c">Bruno Henrique Alves</span>
-            <span className="col-idade-c">39 anos</span>
-            <span className="col-status-c">Atendimento</span>
-            <span className="col-categoria-c">Beneficiário</span>
-            <Link className="col-detalhes-c link_fake" to="#">ver mais</Link>
-          </div>
-
-          <div className="linha_usuario destaque">
-            <span className="col-nome-c">Carla Mendes Ribeiro</span>
-            <span className="col-idade-c">52 anos</span>
-            <span className="col-status-c">Ativo</span>
-            <span className="col-categoria-c">Funcionario</span>
-            <Link className="col-detalhes-c link_fake" to="#">ver mais</Link>
-          </div>
-
-          <div className="linha_usuario">
-            <span className="col-nome-c">Juliana Pereira Santos</span>
-            <span className="col-idade-c">26 anos</span>
-            <span className="col-status-c">Encaminhado</span>
-            <span className="col-categoria-c">Beneficiário</span>
-            <Link className="col-detalhes-c link_fake" to="#">ver mais</Link>
-          </div>
-
-          <div className="linha_usuario destaque">
-            <span className="col-nome-c">Felipe Rocha Martins</span>
-            <span className="col-idade-c">34 anos</span>
-            <span className="col-status-c">Atendimento</span>
-            <span className="col-categoria-c">Beneficiário</span>
-            <Link className="col-detalhes-c link_fake" to="#">ver mais</Link>
-          </div>
-
-          <div className="linha_usuario">
-            <span className="col-nome-c">Patricia Gomes Duarte</span>
-            <span className="col-idade-c">47 anos</span>
-            <span className="col-status-c">Ativo</span>
-            <span className="col-categoria-c">Funcionario</span>
-            <Link className="col-detalhes-c link_fake" to="#">ver mais</Link>
-          </div>
-
-          <div className="linha_usuario destaque">
-            <span className="col-nome-c">Lucas Andrade Silva</span>
-            <span className="col-idade-c">22 anos</span>
-            <span className="col-status-c">Encaminhado</span>
-            <span className="col-categoria-c">Beneficiário</span>
-            <Link className="col-detalhes-c link_fake" to="#">ver mais</Link>
-          </div>
-
-          <div className="linha_usuario">
-            <span className="col-nome-c">Fernanda Lopes Carvalho</span>
-            <span className="col-idade-c">36 anos</span>
-            <span className="col-status-c">Finalizado</span>
-            <span className="col-categoria-c">Beneficiário</span>
-            <Link className="col-detalhes-c link_fake" to="#">ver mais</Link>
-          </div>
-
-          <div className="linha_usuario destaque">
-            <span className="col-nome-c">Thiago Barros Nogueira</span>
-            <span className="col-idade-c">41 anos</span>
-            <span className="col-status-c">Atendimento</span>
-            <span className="col-categoria-c">Beneficiário</span>
-            <Link className="col-detalhes-c link_fake" to="#">ver mais</Link>
-          </div>
-
-        </section>
       </main>
 
       <Footer />
-    </>
+
+    </div>
   );
 }
-
-export default Fcadastro;
